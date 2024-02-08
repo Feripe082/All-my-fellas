00f2fe767427f685f5ce2c2fcb5d9dbb52497004
Luiz <luiz_f_maria@estudante.sesisenai.org.br>
2024-02-08 08:57:13 -0300

    leitura dos dados do formulario

---
+++ voto_main.js
@@ -0,0 +1,13 @@
+let resposta = document.getElementById('resposta')
+let verificar = document.getElementById('verificar')
+let nome = document.getElementById('nome').value
+
+verificar.addEventListener('click', ()=>{
+    let nome = document.getElementById('nome').value
+    let idade = number (document.getElementById('idade').value)
+    let titulo = number (document.getElementById('titulo').value)
+
+    console.log(nome,idade,titulo)
+
+}
+)
